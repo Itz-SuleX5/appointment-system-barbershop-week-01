4453861a39855acf4f6a5ddccae20e35e85ff35b
Itz-SuleX5 <aa23028@ues.edu.sv>
2026-01-03 17:09:41 -0600

    fix: add services ManyToMany field to Combo model

--- Backend/barbershop/models.py
+++ Backend/barbershop/models.py
@@ -23,6 +23,7 @@ class Combo(models.Model):
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     duration = models.DurationField()
+    services = models.ManyToManyField(Service, related_name='combos')
 
     def __str__(self):
         return self.name
