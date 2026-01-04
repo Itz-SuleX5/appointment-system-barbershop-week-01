396d12f54246e6d80b7e2ba8c0873ac918ae80e7
Itz-SuleX5 <aa23028@ues.edu.sv>
2026-01-03 19:15:25 -0600

    fix: enforce admin permissions for Service CRUD endpoints in backend

--- Backend/barbershop/views.py
+++ Backend/barbershop/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from .models import Category, Service, Combo
 from rest_framework import viewsets
 from .serializers import CategorySerializer, ServiceSerializer, ComboSerializer
+from .permissions import IsAdminForUnsafeMethods
 
 # Create your views here.
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminForUnsafeMethods]
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [IsAdminForUnsafeMethods]
 
 class ComboViewSet(viewsets.ModelViewSet):
     queryset = Combo.objects.all()
     serializer_class = ComboSerializer
+    permission_classes = [IsAdminForUnsafeMethods]
